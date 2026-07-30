d20dab99b0410d621d61212a867514b5ae82842e
hiral panchal <hiralazure2025@gmail.com>
2026-07-30 21:25:52 +0100

    initial fastapi project

---
+++ chaimenu/data.py
@@ -0,0 +1,72 @@
+menu_items =[
+    {
+        "id": 1,
+        "name": "masala chai",
+        "category": "biscuits",
+        "description": "spice tea",
+        "available": True
+    },
+    {
+        "id": 2,
+        "name": "dummy item",
+        "category": "dummy",
+        "description": "sample data",
+        "available": False
+    },
+    {
+        "id": 3,
+        "name": "coffee",
+        "category": "beverages",
+        "description": "hot coffee",
+        "available": True
+    },
+    {
+        "id": 4,
+        "name": "sandwich",
+        "category": "snacks",
+        "description": "grilled sandwich",
+        "available": True
+    },
+    {
+        "id": 5,
+        "name": "pizza",
+        "category": "pizza",
+        "description": "cheese pizza",
+        "available": True
+    },
+    {
+        "id": 6,
+        "name": "burger",
+        "category": "fast food",
+        "description": "veg burger",
+        "available": False
+    },
+    {
+        "id": 7,
+        "name": "pasta",
+        "category": "main course",
+        "description": "tomato pasta",
+        "available": True
+    },
+    {
+        "id": 8,
+        "name": "fries",
+        "category": "snacks",
+        "description": "crispy fries",
+        "available": True
+    },
+    {
+        "id": 9,
+        "name": "juice",
+        "category": "beverages",
+        "description": "fresh orange juice",
+        "available": True
+    },
+    {
+        "id": 10,
+        "name": "salad",
+        "category": "starter",
+        "description": "green salad",
+        "available": False
+    }
+]
---
+++ chaimenu/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI,Query,HTTPException
+from models import MenuItem,MenuResponse
+from data import menu_items
+app = FastAPI(title="chai point menu API",description="Read only menu api")
+
+@app.get("/")
+def root():
+    return {"message":"welcome to chai point menu api"}
+
+@app.get("/menu",response_model=MenuResponse)
+def get_menu(category: str|None =Query(None,description="Filter by chai,snack or fast food")):
+    if category:
+        filtered =[item for item in menu_items if item["category"]==category.lower()]
+        if not filtered:
+            raise HTTPException(status_code=404,detail=f"No item found in category:{category}")
+        return MenuResponse(count = len(filtered),items=filtered)
+    return MenuResponse(count=len(menu_items),items=menu_items)
+
+@app.get("/menu/{item_id}",response_model=MenuItem)
+def get_item(item_id: int):
+    for item in  menu_items:
+        if item["id"]==item_id:
+            return item
+    raise HTTPException(status_code=404,detail=f"Menu item with id {item_id} not found")
